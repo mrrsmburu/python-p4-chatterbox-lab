da08c411d7bac767f56e726f18943351ffa1a15e
mrrsmburu <mrrsmburu@gmail.com>
2024-07-07 21:26:43 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -16,11 +16,74 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return jsonify([{
+            'id': message.id,
+            'username': message.username,
+            'body': message.body,
+            'created_at': message.created_at,
+            'updated_at': message.updated_at
+        } for message in messages]), 200
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        body = data.get('body')
+
+        if not username or not body:
+            return make_response(jsonify({"error": "Username and body are required"}), 422)
+
+        new_message = Message(username=username, body=body)
+        db.session.add(new_message)
+        db.session.commit()
+
+        return jsonify({
+            'id': new_message.id,
+            'username': new_message.username,
+            'body': new_message.body,
+            'created_at': new_message.created_at,
+            'updated_at': new_message.updated_at
+        }), 201
+
+    
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+
+    if request.method == 'GET':
+        return jsonify({
+            'id': message.id,
+            'username': message.username,
+            'body': message.body,
+            'created_at': message.created_at,
+            'updated_at': message.updated_at
+        }), 200
+
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        body = data.get('body')
+
+        if not body:
+            return make_response(jsonify({"error": "Body is required"}), 422)
+
+        message.body = body
+        db.session.commit()
+
+        return jsonify({
+            'id': message.id,
+            'username': message.username,
+            'body': message.body,
+            'created_at': message.created_at,
+            'updated_at': message.updated_at
+        }), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
+
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -12,3 +12,10 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    def __repr__(self):
+        return f"<Message {self.id}: {self.username} - {self.body[:30]}>"
